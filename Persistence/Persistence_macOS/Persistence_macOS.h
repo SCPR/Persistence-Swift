295c656b921a0f1e5c57de97fe18df7329c3d6ff
Jeff Campbell <jcampbell@scpr.org>
2019-04-10 00:13:37 -0700

    Added macOS build target…
    Signed-off-by: Jeff Campbell <jcampbell@scpr.org>

---
+++ Persistence/Persistence_macOS/Persistence_macOS.h
@@ -0,0 +1,19 @@
+//
+//  Persistence_macOS.h
+//  Persistence_macOS
+//
+//  Created by Jeff on 4/9/19.
+//  Copyright © 2019 Southern California Public Radio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Persistence_macOS.
+FOUNDATION_EXPORT double Persistence_macOSVersionNumber;
+
+//! Project version string for Persistence_macOS.
+FOUNDATION_EXPORT const unsigned char Persistence_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Persistence_macOS/PublicHeader.h>
+
+
